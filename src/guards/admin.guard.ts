8c4703a54f925af69b0df502a21492c44b6027cc
Classbinu <classbinu@gmail.com>
2024-05-01 23:55:38 +0900

    Comment: Todo Admin Guard

--- src/guards/admin.guard.ts
+++ src/guards/admin.guard.ts
@@ -13,6 +13,7 @@ export class AdminGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const userId = request.user.userId;
 
+    // Todo: 권한 제어 하드코딩이 아닌 DB에서 가져오도록 수정
     const adminList = [1];
 
     if (!adminList.includes(userId)) {
