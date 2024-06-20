9602c302476869d352750ae83ccd220f500e04a4
classbinu <classbinu@example.com>
2024-06-20 16:17:56 +0900

    Feat: DB Master-Slave

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -11,14 +11,28 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         type: 'postgres',
-        host: config.get<string>('db.host'),
-        username: config.get<string>('db.username'),
-        password: config.get<string>('db.password'),
-        port: config.get<number>('db.port'),
+        replication: {
+          master: {
+            host: config.get<string>('db.host'),
+            username: config.get<string>('db.username'),
+            password: config.get<string>('db.password'),
+            database: config.get<string>('db.name'),
+            port: config.get<number>('db.port'),
+          },
+          slaves: [
+            {
+              host: config.get<string>('slaveDb.host'),
+              username: config.get<string>('slaveDb.username'),
+              password: config.get<string>('slaveDb.password'),
+              database: config.get<string>('slaveDb.name'),
+              port: config.get<number>('slaveDb.port'),
+            },
+          ],
+        },
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         subscribers: ['post.subscriber.ts'],
         synchronize: true,
-        logging: ['query', 'error', 'schema'],
+        logging: true,
       }),
     }),
   ],
